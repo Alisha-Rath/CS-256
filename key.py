81e67b1e84a96f15302fa4be0e5210d30685973e
manvendra777 <56351311+manvendra777@users.noreply.github.com>
2024-09-29 00:58:12 -0700

    Update key.py

--- key.py
+++ key.py
@@ -1,3 +1,3 @@
 #key = ''
-key = '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'
+key = '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'
 # Enter your key here from openrouter
